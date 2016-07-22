2094a054bd82ca6422cfed2104e8bb682ea6004e
Jakub Tomanik <j.tomanik@gmail.com>
2016-07-22 13:43:55 +0200

    added ependencies

--- Package.swift
+++ Package.swift
@@ -1,5 +1,21 @@
 import PackageDescription
 
 let package = Package(
-    name: "PokePartyBackend"
+    name: "PokePartyBackend",
+    targets: [
+        Target(
+            name: "PokePartyBackend",
+            dependencies: [])
+    ],
+    dependencies: [
+        .Package(url: "https://github.com/qutheory/libc.git", majorVersion: 0, minor: 1),
+        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor: 19),
+        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 0, minor: 9),
+        .Package(url: "https://github.com/jtomanik/Environment.git", majorVersion: 0, minor: 3),
+        .Package(url: "https://github.com/tooploox/Kitura-HealthCheck.git", majorVersion: 0, minor: 2),
+        .Package(url: "https://github.com/Zewo/Mustache.git", majorVersion: 0, minor: 6),
+        .Package(url: "https://github.com/czechboy0/Redbird.git", majorVersion: 0, minor: 7),
+        .Package(url: "https://github.com/jtomanik/Promissum.git", majorVersion: 0, minor: 6)
+    ],
+    exclude: ["Makefile", "Kitura-Build"]
 )
